98fb6cc6fed82ca8ef94f66dc0d5bd312bf36784
Ko Min <winkomin@gmail.com>
2014-07-18 16:55:40 +0800

    Update script.js
    1. Update jQuery codes

--- js/script.js
+++ js/script.js
@@ -1,163 +1,42 @@
-$(function () {
-
-
-			$("body").removeClass('no-js');
-			$(document).ready(function() {
-				var ifont = $("select#ifont").val();
-				$("#input").css("font-family",ifont);
-				switch(ifont) {
-					case 'WinInnwa':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					case 'M-myanmar':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					case 'Kannaka':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					case 'NLD1':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;						
-					default:
-						$("div#ascii-controls").addClass('hide-ascii-controls');
-					break;
+$(document).ready(function()
+{
+	$('#time_container').hide();
+	$('form#converter').submit(function(e)
+	{
+		var formData = $(this).serialize();
+		e.preventDefault();
+		if($('#ifont').val()=="")
+		{
+			alert('Please select input font!');
+			return 0;
+		}
+		if($('#input').val()=="")
+		{
+			return 0;
+		}
+		$.ajax({
+			type: 'post',
+			url: 'ajax.php',
+			data: $('form#converter').serialize(),
+			dataType: 'json',
+			cache: false,
+			success: function (data) 
+			{
+				output_text = data.output_text;
+				if(data.output_text){
+				$('#output').text(data.output_text);
+				$('#ajax_output').text(data.output_text);
 				}
-				var ofont = $("select#ofont").val();
-				$("#output").css("font-family",ofont);
-			});
-			$("select#ifont").change(function() {
-				var ifont = $("select#ifont").val();
-				$("#input").css("font-family",ifont);
-				switch(ifont) {
-					case 'WinInnwa':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					case 'M-myanmar':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					case 'Kannaka':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					case 'NLD1':
-						$("div#ascii-controls").removeClass('hide-ascii-controls');
-					break;
-					default:
-						$("div#ascii-controls").addClass('hide-ascii-controls');
-					break;
-				}
-			});
-
-			$("select#ofont").change(function() {
-				var ofont = $("select#ofont").val();
-				$("#output").css("font-family",ofont);
-
-			});
-
-			$("form#converter").submit(function(){
-				$("#output").empty();
-				$("#loading_animation").bind({
-				ajaxStart: function() { $(this).show(); },
-				ajaxStop: function() { $(this).hide(); }
-				});
-				var ofont = $("select#ofont").val();
-				$("#output").css("font-family",ofont);
-			});
-
-			//$("input#inputfile").remove();
-
-/*
-			    $('#inputfile').fileupload({
-			    	url: 'ajax.php',
-			        dataType: 'json',
-			        add: function (e, data) {
-			            data.context = $("#submit")
-			                .click(function () {
-			                    data.context = $('#progress').text('Uploading...');
-			                    data.submit();
-			                });
-			        },
-			        progressall: function (e, data) {
-				        var progress = parseInt(data.loaded / data.total * 100, 10);
-				        $('#progress .bar').css(
-				            'width',
-				            progress + '%'
-				        );
-				    },
-			        done: function (e, data) {
-			            $('#progress').text('Upload finished.');
-			        }
-			    });
-			    */
-			$('form#converter').submit(function(e) {
-					$.ajax({
-					type: 'post',
-					url: 'ajax.php',
-					data: $('form#converter').serialize(),
-					dataType: 'json',
-					cache: false,
-					success: function (data) {
-						//{"ofont_family":"Padauk, MyanmarText, Tharlon, Myanmar3","ochecked":"uni","ifont_family":"Ayar","ichecked":"ay","output_text":"asdf"}
-						output_text = data.output_text;
-						if(data.output_text){
-						$('#output').text(data.output_text);
-						$('#ajax_output').text(data.output_text);
-						}
-						$("#output").css("font-family",data.ofont_family);
-						$("#ajax_output").css("font-family",data.ofont_family);
-						$("textarea#input").css("font-family",data.ifont_family);
-						var ofont_selector = "#ofont #"+data.ochecked;
-						var ifont_selector = "#ifont #"+data.ochecked;
-						$(ofont_selector).attr('selected');
-						$(ifont_selector).attr('selected');
-/*
-                        if(data.ochecked == 'uni'){
-                        	$("#ofont #uni").attr('selected');
-                        }
-                        if(data.ochecked == 'ay'){
-                        	$("#ofont #ay").attr('selected');
-                        }
-                        if(data.ochecked == 'zg'){
-                        	$("#ofont #zg").attr('selected');
-                        }
-                        if(data.ichecked == 'uni'){
-                        	$("#ifont #uni").attr('selected');
-                        }
-                        if(data.ichecked == 'ay'){
-                        	$("#ifont #ay").attr('selected');
-                        }
-                        if(data.ichecked == 'zg'){
-                        	$("#ifont #zg").attr('selected');
-                        }
-                        if(data.ichecked == 'auto'){
-                        	$("#ifont #auto").attr('selected');
-                        }
-*/
-                        if(data.error_message || data.type_error){
-							$('#error').text(data.error_message);
-							$('#error').html('<br>');
-							$('#error').text(data.type_error);
-							$('#myModal').modal('show');
-						}
-						if(data.time){
-							$('#message').text(data.time);
-							$('div').removeClass('hide-time');
-						}
-						//alert(output);
-                    }
-                });
-
-                e.preventDefault();
-            });
-				$('#myTabContent a[href="#home"]').tab('show');
-				$('#myTabContent a[href="#uploadsect"]').tab('show');
-				$('#myTabContent a[href="#about"]').tab('show');
-				$('#myTabContent a[href="#help"]').tab('show');
-/*
-				$('#ajax_output').focus(function() {
-					textarea['ajax_output'].document.execCommand('copy', false, null);
-				});
-*/
-				var inputHeight = $('textarea#input').css('height');
-				$('#output').css('height',inputHeight);
-				$('#ajax_output').css('height',inputHeight);
- });
+				$('#convert_time').text(data.time);
+				$("#output").css("font-family",data.ofont_family);
+				$("#ajax_output").css("font-family",data.ofont_family);
+				$("textarea#input").css("font-family",data.ifont_family);
+				var ofont_selector = "#ofont #"+data.ochecked;
+				var ifont_selector = "#ifont #"+data.ochecked;
+				$(ofont_selector).attr('selected');
+				$(ifont_selector).attr('selected');
+				$('#time_container').show();
+	        }
+	    });
+	});
+});
